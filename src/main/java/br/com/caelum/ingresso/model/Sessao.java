f71e03d042c5411bc209244bc9190f791442e626
Danilo Miyamura <danilo.miyamura@gmail.com>
2020-11-12 06:58:25 -0300

    Adicionando página Sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,10 +2,23 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private LocalTime horarioDeInicio;
+	
+	@ManyToOne
 	private Filme filme;
+	
+	@ManyToOne
 	private Sala sala;
 
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
